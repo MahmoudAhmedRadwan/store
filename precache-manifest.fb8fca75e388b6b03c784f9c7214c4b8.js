6d7b3e19cbca254e87cafe831328860f4c4e2237
Mahmoud Ahmed <mahmoudradwan2625@gmail.com>
2019-11-27 00:55:17 +0200

    Updates

---
+++ precache-manifest.fb8fca75e388b6b03c784f9c7214c4b8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8310c7143d1ee8b461e95730c712db62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eea2504571f86fb442c1",
+    "url": "/static/css/2.b883d912.chunk.css"
+  },
+  {
+    "revision": "9a8414ffaf73ac1d4a4b",
+    "url": "/static/css/main.f47271fc.chunk.css"
+  },
+  {
+    "revision": "eea2504571f86fb442c1",
+    "url": "/static/js/2.6ede0c4d.chunk.js"
+  },
+  {
+    "revision": "9a8414ffaf73ac1d4a4b",
+    "url": "/static/js/main.f207f9b0.chunk.js"
+  },
+  {
+    "revision": "f1df727505001623e465",
+    "url": "/static/js/runtime-main.9a4dad58.js"
+  },
+  {
+    "revision": "b4f625d5c680183ab92a6aec55284a12",
+    "url": "/static/media/bmw-background.b4f625d5.jpg"
+  },
+  {
+    "revision": "a6610109b44bd4e0a4fe3972d904aede",
+    "url": "/static/media/bmw.a6610109.jpg"
+  },
+  {
+    "revision": "f1d02c71692472a6d5673cd61d610b60",
+    "url": "/static/media/fiat2019.f1d02c71.jpg"
+  },
+  {
+    "revision": "e47f350cdee89271efcd49439dbfe839",
+    "url": "/static/media/fiat2020.e47f350c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "de4b50f889f80f3a7826c083cdaaf3db",
+    "url": "/static/media/kia2018.de4b50f8.jpg"
+  },
+  {
+    "revision": "b2700d30d3397e050b7702d2ab0799f2",
+    "url": "/static/media/kia2019.b2700d30.jpg"
+  },
+  {
+    "revision": "50f624d208b869155e9b79c0d8a54b20",
+    "url": "/static/media/kia2020.50f624d2.jpg"
+  },
+  {
+    "revision": "eca3a33ad94f99598351d7482ad106b6",
+    "url": "/static/media/lam.eca3a33a.jpg"
+  },
+  {
+    "revision": "c7959f649def8b39167268d826a8f9ae",
+    "url": "/static/media/mercedes2019.c7959f64.jpg"
+  },
+  {
+    "revision": "463eab11659cfaa1a109d018d791b5ec",
+    "url": "/static/media/mercedes2020.463eab11.jpg"
+  },
+  {
+    "revision": "ae9698f65d2ffededd7bfaafd845507f",
+    "url": "/static/media/toyota2018.ae9698f6.jpg"
+  },
+  {
+    "revision": "fac2bffeacc0a6e8bd17fbe02a40ed5e",
+    "url": "/static/media/toyota2019.fac2bffe.jpg"
+  },
+  {
+    "revision": "027f940cdf5e1e5e6a178ab44fbbe52e",
+    "url": "/static/media/toyota2020.027f940c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/store/precache-manifest.6e0acb83ad1fe6cd0ff63389954e8eba.js"
+  "/precache-manifest.fb8fca75e388b6b03c784f9c7214c4b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/store/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
